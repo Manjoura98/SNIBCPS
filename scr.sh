a919954f7c5ad5af93f6ddc85d24681cf55b87d9
Mehdi Manjoura <89964355+Manjoura98@users.noreply.github.com>
2023-08-08 13:22:07 -0400

    Update scr.sh

--- scr.sh
+++ scr.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --time=0-00:30:00                                  # Runtime is sam output/hour, see below, format --time=day-hours:minutes
-###SBATCH --nodes=1                                     # In case all the tasks need to be on the same node
-#SBATCH --ntasks=4                                      # Increase per sample would require other bowtie settings!
-#SBATCH --mem-per-cpu 8000                              # Samtools uses ?, GB comes with 4 or 8GB per CPU
-#SBATCH --account=def-pcampeau                  # Not your own account
-#SBATCH -o GEAanalysisJune14H3.3_FreshHemic_E15vsIgG1_E15.out                  # See https://genome.ucsc.edu/goldenPath/help/bam.html for sam>>
-#SBATCH -e GEAanalysisJune14H3.3_FreshHemic_E15vsIgG1.err                  # Give names to output prefix (here: toolsjobFeb4 and toolsFeb4>
+#SBATCH --time=0-00:30:00                                  
+###SBATCH --nodes=1                                     
+#SBATCH --ntasks=4                                      
+#SBATCH --mem-per-cpu 8000                              
+#SBATCH --account=def-pcampeau                  
+#SBATCH -o GEAanalysisJune14H3.3_FreshHemic_E15vsIgG1_E15.out                  
+#SBATCH -e GEAanalysisJune14H3.3_FreshHemic_E15vsIgG1.err                  
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mehdi.manjoura@umontreal.ca
 
@@ -28,4 +28,4 @@ python updatedCode.py
 
 
 
-#scp mehdim98@narval.computecanada.ca:scratch/codes/pipe/Module1Graphs/* /home/mehdim/Desktop/Directoryname/
+
